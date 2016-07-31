6bfa9875b1b6339fae810626926ccdf0849d03a8
takahashi akisato <akisato@takahashi-no-MacBook-Air-2.local>
2016-07-31 16:21:25 +0900

    Add gitignore

--- SenYo/AppDelegate.swift
+++ SenYo/AppDelegate.swift
@@ -12,11 +12,17 @@ import CoreData
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var navigationController: UINavigationController?
     var window: UIWindow?
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        let first: ViewController = ViewController()
+        navigationController = UINavigationController(rootViewController: first)
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        self.window?.rootViewController = navigationController
+        self.window?.makeKeyAndVisible()
+        
         return true
     }
 
