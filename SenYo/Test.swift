5554a2327800660485e375017a4de76a520cdbff
HyumaMatsue <gibsonjr.k@gmail.com>
2016-08-27 15:25:46 +0900

    Test

---
+++ SenYo/Test.swift
@@ -0,0 +1,26 @@
+//
+//  Test.swift
+//  SenYo
+//
+//  Created by 松江飛雄馬 on 2016/08/27.
+//  Copyright © 2016年 takahashi akisato. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Test : UIView{
+
+    
+        
+        required init(coder aDecoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        required init() {
+            super.init(frame: CGRectMake(0, 0, 0, 0));
+            
+        }
+        
+    
+}
